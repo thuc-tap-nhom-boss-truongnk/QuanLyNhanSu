3deafadeaf826afd9ed764ae802f661eee241c60
truongnk4365 <truongnk6879@gmail.com>
2018-05-12 10:24:51 +0700

    service connect database qlns

---
+++ IService1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ManageStaffServiceWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //1. nhan vien
+        [OperationContract]
+        DataSet ShowNhanVien();
+
+        [OperationContract]
+        string AddNhanVien(NhanVien nv);
+
+        [OperationContract]
+        bool DeleteNhanVien(NhanVien nv);
+
+        [OperationContract]
+        void UpdateNhanVien(NhanVien nv);
+
+        //2. phong ban
+        [OperationContract]
+        DataSet ShowPhongBan();
+
+        [OperationContract]
+        string AddPhongBan(PhongBan pb);
+
+        [OperationContract]
+        bool DeletePhongBan(PhongBan pb);
+
+        [OperationContract]
+        void UpdatePhongBan(PhongBan pb);
+
+        //3. chuc vu
+        [OperationContract]
+        DataSet ShowChucVu();
+
+        [OperationContract]
+        string AddChucVu(ChucVu cv);
+
+        [OperationContract]
+        bool DeleteChucVu(ChucVu cv);
+
+        [OperationContract]
+        void UpdateChucVu(ChucVu cv);
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+
+    //1. nhan vien
+    [DataContract]
+    public class NhanVien
+    {
+        int id;
+        string name;
+        DateTime dob;
+        string gender;
+        string phone;
+        string address;
+        string cmt;
+        string level;
+        int idcv;
+        int idpb;
+
+        [DataMember]
+        public int NVid
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string NVname
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public DateTime NVdob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        [DataMember]
+        public string NVgender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        [DataMember]
+        public string NVphone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        [DataMember]
+        public string NVaddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        [DataMember]
+        public string NVcmt
+        {
+            get { return cmt; }
+            set { cmt = value; }
+        }
+
+        [DataMember]
+        public string NVlevel
+        {
+            get { return level; }
+            set { level = value; }
+        }
+
+        [DataMember]
+        public int NVidcv
+        {
+            get { return idcv; }
+            set { idcv = value; }
+        }
+
+        [DataMember]
+        public int NVidpb
+        {
+            get { return idpb; }
+            set { idpb = value; }
+        }
+    }
+
+    //2. phong ban
+    [DataContract]
+    public class PhongBan
+    {
+        int id;
+        string name;
+
+        [DataMember]
+        public int PBid
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string PBname
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    //3. chuc vu
+    [DataContract]
+    public class ChucVu
+    {
+        int id;
+        string name;
+
+        [DataMember]
+        public int CVid
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string CVname
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ManageStaffServiceWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        string strQuery = @"Data Source=.\SQLEXPRESS;Initial Catalog=QuanLyNhanVien;Persist Security Info=True;User ID=sa;Password=1234567";
+
+    // 1. nhan vien
+        public string AddNhanVien(NhanVien nv)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into nhanvien(name,birth,gender,phone,address,cmt,level,idcv,idpb) values(@name,@birth,@gender,@phone,@address,@cmt,@level,@idcv,@idpb)", con);
+            cmd.Parameters.AddWithValue("@name", nv.NVname);
+            cmd.Parameters.AddWithValue("@birth", nv.NVdob);
+            cmd.Parameters.AddWithValue("@gender", nv.NVgender);
+            cmd.Parameters.AddWithValue("@phone", nv.NVphone);
+            cmd.Parameters.AddWithValue("@address", nv.NVaddress);
+            cmd.Parameters.AddWithValue("@cmt", nv.NVcmt);
+            cmd.Parameters.AddWithValue("@level", nv.NVlevel);
+            cmd.Parameters.AddWithValue("@idcv", nv.NVidcv);
+            cmd.Parameters.AddWithValue("@idpb", nv.NVidpb);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = nv.NVname + "them thanh cong";
+            }
+            else
+            {
+                Message = nv.NVname + "them khong thanh cong";
+            }
+            con.Close();
+            return Message;
+        }
+
+        public bool DeleteNhanVien(NhanVien nv)
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from nhanvien where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", nv.NVid);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return true;
+        }
+
+        public DataSet ShowNhanVien()
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select dbo.nhanvien.id, dbo.nhanvien.name, birth, gender,phone,address, cmt,level, dbo.chucvu.name, dbo.phongban.name from dbo.nhanvien, dbo.chucvu,dbo.phongban where chucvu.id=nhanvien.idcv and nhanvien.idpb=phongban.id", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+
+        public void UpdateNhanVien(NhanVien nv)
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update  nhanvien set name=@name, birth=@birth, gender=@gender, phone=@phone, address=@address, cmt=@cmt, level=@level, idcv=@idcv, idpb=@idpb where id=@id ");
+            cmd.Parameters.AddWithValue("@id", nv.NVid);
+            cmd.Parameters.AddWithValue("@name", nv.NVname);
+            cmd.Parameters.AddWithValue("@birth", nv.NVdob);
+            cmd.Parameters.AddWithValue("@gender", nv.NVgender);
+            cmd.Parameters.AddWithValue("@phone", nv.NVphone);
+            cmd.Parameters.AddWithValue("@address", nv.NVaddress);
+            cmd.Parameters.AddWithValue("@cmt", nv.NVcmt);
+            cmd.Parameters.AddWithValue("@level", nv.NVlevel);
+            cmd.Parameters.AddWithValue("@idcv", nv.NVidcv);
+            cmd.Parameters.AddWithValue("@idpb", nv.NVidpb);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        // end nhan vien
+
+
+        // 2. phong ban
+        public DataSet ShowPhongBan()
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from phongban", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+
+       
+
+        public string AddPhongBan(PhongBan pb)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into phongban(name) values(@name)", con);
+            cmd.Parameters.AddWithValue("@name", pb.PBname);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = pb.PBname + "them thanh cong";
+            }
+            else
+            {
+                Message =pb.PBname + "them khong thanh cong";
+            }
+            con.Close();
+            return Message;
+        }
+
+        public bool DeletePhongBan(PhongBan pb)
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from phongban where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", pb.PBid);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return true;
+        }
+
+        public void UpdatePhongBan(PhongBan pb)
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update phongban set name=@name where id=@id ");
+            cmd.Parameters.AddWithValue("@id", pb.PBid);
+            cmd.Parameters.AddWithValue("@name", pb.PBname);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        // 2. chuc vu
+        public DataSet ShowChucVu()
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from chucvu", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return ds;
+        }
+
+        public string AddChucVu(ChucVu cv)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into chucvu (name) values (@name)", con);
+            cmd.Parameters.AddWithValue("@name",cv.CVname);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = cv.CVname + "them thanh cong";
+            }
+            else
+            {
+                Message = cv.CVname + "them khong thanh cong";
+            }
+            con.Close();
+            return Message;
+        }
+
+        public bool DeleteChucVu(ChucVu cv)
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from chucvu where id=@id", con);
+            cmd.Parameters.AddWithValue("@id",cv.CVid);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return true;
+        }
+
+        public void UpdateChucVu(ChucVu cv)
+        {
+            SqlConnection con = new SqlConnection(strQuery);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update chucvu set name=@name where id=@id ");
+            cmd.Parameters.AddWithValue("@id", cv.CVid);
+            cmd.Parameters.AddWithValue("@name", cv.CVname);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
